cd04126b6ab51f9d4e5dd7b83d843aca9733c677
Varun Ratnakar <varunratnakar@gmail.com>
2020-08-05 09:59:15 +0530

    disable dev mode

--- mint-ensemble-manager/src/config/app.ts
+++ mint-ensemble-manager/src/config/app.ts
@@ -1,6 +1,6 @@
 export const PORT = 3000;
 export const VERSION = "v1";
 
-export const DEVMODE = true;
+export const DEVMODE = false;
 // We need a code, logs and data directory under the DEVHOMEDIR
 export const DEVHOMEDIR = "/Users/varun/mintproject"; 
