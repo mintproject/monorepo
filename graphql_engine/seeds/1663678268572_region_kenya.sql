3dea34e585f1b9f7d41b41b6a4d0bcc371eb733e
Maximiliano Osorio <maxiosorio@gmail.com>
2022-10-11 11:38:12 -0300

    Revert "fix: delete old regions"
    This reverts commit 97ccba097bccec880a0b9a5905126c5764a56de0.

---
+++ graphql_engine/seeds/1663678268572_region_kenya.sql
@@ -0,0 +1,1 @@
+INSERT INTO public.region (id, name, parent_region_id, category_id, model_catalog_uri) VALUES ('kenya', 'Kenya', NULL, NULL, 'https://w3id.org/okn/i/mint/Kenya');
---
+++ graphql_engine/seeds/1663678268575_region_somalia.sql
@@ -0,0 +1,1 @@
+INSERT INTO public.region (id, name, parent_region_id, category_id, model_catalog_uri) VALUES ('somalia', 'Somalia', NULL, NULL, 'https://w3id.org/okn/i/mint/Somalia');
