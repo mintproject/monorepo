c6980ae363a869d82f827fc01fd2f60bac2ce3ea
Maximiliano Osorio <maxiosorio@gmail.com>
2025-07-10 12:41:55 -0400

    fix: obtain the thread_model.id (#104)

--- mint-ensemble-manager/src/api/api-v1/services/executionsTapisService.ts
+++ mint-ensemble-manager/src/api/api-v1/services/executionsTapisService.ts
@@ -1,11 +1,11 @@
-import { getThread } from "@/classes/graphql/graphql_functions";
-import { Thread } from "@/classes/mint/mint-types";
 import { ModelThread } from "@/classes/api";
 import { TapisExecutionService } from "@/classes/tapis/adapters/TapisExecutionService";
 import { ExecutionCreation } from "@/classes/common/ExecutionCreation";
 import { getConfiguration } from "@/classes/mint/mint-functions";
 import { getTokenFromAuthorizationHeader } from "@/utils/authUtils";
 import { NotFoundError } from "@/classes/common/errors";
+import { threadFromGQL } from "@/classes/graphql/graphql_adapter";
+import { getThread } from "@/classes/graphql/graphql_functions_v2";
 
 export interface ExecutionsTapisService {
     submitExecution(threadmodel: ModelThread, token: string): Promise<string[]>;
@@ -19,7 +19,8 @@ const executionsTapisService = {
         }
         const prefs = getConfiguration();
         const TapisExecution = new TapisExecutionService(token, prefs.tapis.basePath);
-        const thread: Thread = await getThread(threadmodel.thread_id);
+        const threadResponse = await getThread(threadmodel.thread_id);
+        const thread = threadFromGQL(threadResponse);
         if (thread) {
             const executionCreation = new ExecutionCreation(
                 thread,
@@ -28,6 +29,13 @@ const executionsTapisService = {
                 token
             );
             await executionCreation.prepareExecutions();
+            // Find the thread model id for the given model id
+            const threadModelId = threadResponse.thread_models.find(
+                (thread_model) => thread_model.model_id === threadmodel.model_id
+            )?.id;
+            if (!threadModelId) {
+                throw new NotFoundError("Thread model not found");
+            }
             if (executionCreation.executionToBeRun.length > 0) {
                 console.log("Execution to be run", executionCreation.executionToBeRun.length);
                 const jobIds = await TapisExecution.submitExecutions(
@@ -36,7 +44,7 @@ const executionsTapisService = {
                     executionCreation.threadRegion,
                     executionCreation.component,
                     thread.id,
-                    threadmodel.thread_id
+                    threadModelId
                 );
                 return jobIds;
             } else {
