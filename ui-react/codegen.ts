9b7899b6102862610b0586c48d0c9a95b058e3b9
Maximiliano Osorio <mosorio@isi.edu>
2026-06-06 20:47:48 -0400

    feat(registration): GetModelFamilies query + CreateModelFamily/CreateConfiguration mutations

--- ui-react/codegen.ts
+++ ui-react/codegen.ts
@@ -11,6 +11,7 @@ const config: CodegenConfig = {
     },
   ],
   documents: 'src/graphql/**/*.graphql',
+  ignoreNoDocuments: true,
   generates: {
     'src/graphql/generated/graphql.ts': {
       plugins: [
@@ -26,7 +27,9 @@ const config: CodegenConfig = {
           uuid: 'string',
           timestamptz: 'string',
           jsonb: 'unknown',
+          _text: 'string[]',
         },
+        skipDocumentsValidation: true,
       },
     },
   },
